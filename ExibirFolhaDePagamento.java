07f90745f5fd8d3a317a27a489329bd76b50cd31
Carlos Eduardo <67875731+CarlosEddie@users.noreply.github.com>
2020-07-20 16:30:53 -0300

    Update ExibirFolhaDePagamento.java

--- ExibirFolhaDePagamento.java
+++ ExibirFolhaDePagamento.java
@@ -3,18 +3,21 @@ public class ExibirFolhaDePagamento {
 
 	public void exibirFolha() {
 		if(Funcionario.funcionarios.size() == 0) {
-			System.out.println("Não a funcionarios cadastrados");
-			Menu.menuInicial();
+			System.out.println("NÃ£o a funcionarios cadastrados");
 		} else {
-			System.out.println("Nome|Cargo|Salário Base|Qtde. Gratificações|Valor das Grat.|"
+			System.out.println("Nome|Cargo|SalÃ¡rio Base|Qtde. GratificaÃ§Ãµes|Valor das Grat.|"
 					+ "Vencimento Mensal");
 			for(int c = 0; c < Funcionario.funcionarios.size(); c++) {
-				System.out.println(Funcionario.funcionarios.get(c) + 
-						Gratificacao.gratDeFuncionarios.get(Funcionario.funcionarios.get(c)));
+				if(Gratificacao.gratDeFuncionarios.get(Funcionario.funcionarios.get(c)) == null) {
+					System.out.println(Funcionario.funcionarios.get(c) + " | 0 | 0 |  " +
+							Funcionario.salarios.get(c));
+				} else {
+					System.out.println(Funcionario.funcionarios.get(c) + 
+							Gratificacao.gratDeFuncionarios.get(Funcionario.funcionarios.get(c)));
+				}
 			}
-			Menu.menuInicial();
 		}
-		
+		Menu.menuInicial();
 	}
 
 }
